2d2a36cb6241c85cd0c2ce56139f008faafe51de
dmitspi <65438771+dmitspi@users.noreply.github.com>
2020-06-18 16:03:55 +0700

    Add files via upload
    экзамен

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = 69.99;
+            var p = Math.Truncate(num);
+            var t = Math.Round(num % (int)num, 2);
+            Console.Write("{0} и {1}", p, t);
+            Console.ReadKey();
+        }
+    }
+}
